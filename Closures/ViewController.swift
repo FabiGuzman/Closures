485b4e8ff665bdc9ced252a29bcbf6d5fb79c636
Fabian <fabi.guzman.gonzalez@gmail.com>
2022-02-23 11:56:18 -0500

    gitignore file on project closures

---
+++ Closures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Closures
+//
+//  Created by Fabian Antonio Guzman Gonzalez on 23/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
